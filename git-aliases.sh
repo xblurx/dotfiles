fbf11d262fc39720fdaba8a4fe54116ef5aaebfb
Vitaly Sharov <55061979+xblurx@users.noreply.github.com>
2023-05-22 12:50:28 +0300

    add git alias script

---
+++ git-aliases.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.s 'status'
+git config --global alias.logf 'log --oneline -n 7'
+git config --global alias.c 'commit'
+git config --global alias.cm 'commit --amend'
