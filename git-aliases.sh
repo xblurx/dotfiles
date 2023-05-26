afcc07e7ce62907875dc963fb826dcee55e64ffb
Vitaly Sharov <v.sharov@bi.zone>
2023-05-26 18:40:43 +0300

    fix git logf alias

--- git-aliases.sh
+++ git-aliases.sh
@@ -2,6 +2,6 @@
 
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.s 'status'
-git config --global alias.logf 'log --oneline -n 7'
+git config --global alias.logf '!git --no-pager log --oneline -n 7' 
 git config --global alias.c 'commit'
 git config --global alias.cm 'commit --amend'
